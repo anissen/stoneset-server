353bc9ae959016b10c7576997789652e1f9561bd
Anders Nissen <andnis@gmail.com>
2018-05-27 20:54:54 +0200

    bug fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const secrets = require('./secrets')
+// const secrets = require('./secrets')
 
 var db
 
