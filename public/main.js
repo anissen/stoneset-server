4c85ee290613debf50018e7210bcdcf05980af79
Anders Nissen <andnis@gmail.com>
2018-05-27 20:44:28 +0200

    Add initial test server

---
+++ public/main.js
@@ -0,0 +1,34 @@
+/* globals fetch */
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        'score': 666,
+        'name': 'Darth Vader',
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('scores', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
